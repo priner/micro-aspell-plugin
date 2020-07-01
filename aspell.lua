f0ba5ee300affec99c4e5a965740d641f40623ea
Ján Priner <johnkie4@gmail.com>
2020-07-02 01:16:27 +0200

    Refactor

--- aspell.lua
+++ aspell.lua
@@ -2,7 +2,7 @@ local micro = import("micro")
 local shell = import("micro/shell")
 local buffer = import("micro/buffer")
 local config = import("micro/config")
-local fmt = import("fmt")
+local util = import("micro/util")
 local utf = import("unicode/utf8")
 
 config.RegisterCommonOption("aspell", "check", "auto")
@@ -15,8 +15,6 @@ function init()
     config.MakeCommand("addpersonal", addpersonal, config.NoComplete)
     config.AddRuntimeFile("aspell", config.RTHelp, "help/aspell.md")
 end
-
-local patterns = {"^# (.-) (%d+)$", "^& (.-) %d+ (%d+): (.+)$"}
 
 local filterModes = {
     xml = "sgml",
@@ -37,7 +35,7 @@ local lock = false
 local next = nil
 
 function runAspell(buf, onExit)
-    local text = fmt.Sprintf("%s", buf.SharedBuffer.LineArray:Bytes())
+    local text = util.String(buf:Bytes())
 
     -- Escape for aspell (it interprets lines that start
     -- with % @ ^ ! etc.)
@@ -69,43 +67,36 @@ function runAspell(buf, onExit)
             nil, onExit, buf)
 end
 
-function spellcheck(bp)
-    if bp ~= nil then
-        local check = bp.Buf.Settings["aspell.check"]
-        if check == "on" or (check == "auto" and filterModes[bp.Buf:FileType()]) then
-            if lock then
-                next = bp
-            else
-                lock = true
-                runAspell(bp.Buf, highlight)
-            end
+function spellcheck(buf)
+    local check = buf.Settings["aspell.check"]
+    if check == "on" or (check == "auto" and filterModes[buf:FileType()]) then
+        if lock then
+            next = buf
         else
-            -- If we aren't supposed to spellcheck, clear the messages
-            bp.Buf:ClearMessages("aspell")
+            lock = true
+            runAspell(buf, highlight)
         end
+    else
+        -- If we aren't supposed to spellcheck, clear the messages
+        buf:ClearMessages("aspell")
     end
 end
 
-function highlight(out, args)
-    local buf = args[1]
+-- Parses the output of Aspell and returns the list of all misspells.
+function parseOutput(out)
+    local patterns = {"^# (.-) (%d+)$", "^& (.-) %d+ (%d+): (.+)$"}
 
     if out:find("command not found") then
         micro.InfoBar():Error(
                 "Make sure that Aspell is installed and available in your PATH")
+        return {}
     elseif not out:find("International Ispell Version") then
         -- Something went wrong, we'll show what Aspell has to say
         micro.InfoBar():Error("Aspell: " .. out)
+        return {}
     end
 
-    lock = false
-
-    buf:ClearMessages("aspell")
-
-    -- This is a hack that keeps the text shifted two columns to the right
-    -- even when no gutter messages are shown
-    local msg = "This message shouldn't be visible (Aspell plugin)"
-    local bmsg = buffer.NewMessageAtLine("aspell", msg, 0, buffer.MTError)
-    buf:AddMessage(bmsg)
+    local misspells = {}
 
     local linenumber = 1
     local lines = split(out, "\n")
@@ -117,24 +108,46 @@ function highlight(out, args)
                 if string.find(line, pattern) then
                     local word, offset, suggestions = string.match(line, pattern)
                     offset = tonumber(offset)
-                    local msg = nil
-                    if suggestions then
-                        msg = word .. " -> " .. suggestions
-                    else
-                        msg = word .. " ->X"
-                    end
+                    local len = utf.RuneCountInString(word)
 
-                    local mlen = utf.RuneCountInString(word)
-                    local mstart = buffer.Loc(offset - 1, linenumber - 1)
-                    local mend = buffer.Loc(offset - 1 + mlen, linenumber - 1)
-                    local bmsg = buffer.NewMessage("aspell", msg, mstart,
-                            mend, buffer.MTWarning)
-                    buf:AddMessage(bmsg)
+                    misspells[#misspells + 1] = {
+                        word = word,
+                        mstart = buffer.Loc(offset - 1, linenumber - 1),
+                        mend = buffer.Loc(offset - 1 + len, linenumber - 1),
+                        suggestions = suggestions and split(suggestions, ", ") or {},
+                    }
                 end
             end
         end
     end
 
+    return misspells
+end
+
+function highlight(out, args)
+    local buf = args[1]
+
+    buf:ClearMessages("aspell")
+
+    -- This is a hack that keeps the text shifted two columns to the right
+    -- even when no gutter messages are shown
+    local msg = "This message shouldn't be visible (Aspell plugin)"
+    local bmsg = buffer.NewMessageAtLine("aspell", msg, 0, buffer.MTError)
+    buf:AddMessage(bmsg)
+
+    for _, misspell in ipairs(parseOutput(out)) do
+        local msg = nil
+        if #(misspell.suggestions) > 0 then
+            msg = misspell.word .. " -> " .. table.concat(misspell.suggestions, ", ")
+        else
+            msg = misspell.word .. " ->X"
+        end
+        local bmsg = buffer.NewMessage("aspell", msg, misspell.mstart,
+                misspell.mend, buffer.MTWarning)
+        buf:AddMessage(bmsg)
+    end
+
+    lock = false
     if next ~= nil then
         spellcheck(next)
         next = nil
@@ -152,395 +165,385 @@ function addpersonal2(out, args)
     local buf = args[1]
     local loc = buf:GetActiveCursor().Loc
 
-    local linenumber = 1
-    local lines = split(out, "\n")
-    for _, line in ipairs(lines) do
-        if line == "" then
-            linenumber = linenumber + 1
-        else
-            for _, pattern in ipairs(patterns) do
-                if string.find(line, pattern) then
-                    local word, offset, suggestions = string.match(line, pattern)
-                    offset = tonumber(offset)
-
-                    local mlen = utf.RuneCountInString(word)
-                    local mstart = buffer.Loc(offset - 1, linenumber - 1)
-                    local mend = buffer.Loc(offset - 1 + mlen, linenumber - 1)
-                    if loc:GreaterEqual(mstart) and loc:LessEqual(mend) then
-                        local options = ""
-                        if buf.Settings["aspell.lang"] ~= "" then
-                            options = options .. " --lang="
-                                    .. buf.Settings["aspell.lang"]
-                        end
-                        if buf.Settings["aspell.dict"] ~= "" then
-                            options = options .. " --master="
-                                    .. buf.Settings["aspell.dict"]
-                        end
-                        if buf.Settings["aspell.args"] ~= "" then
-                            options = options .. " " .. buf.Settings["aspell.args"]
-                        end
-
-                        shell.ExecCommand("sh", "-c", "echo '*" .. word
-                                .. "\n#\n' | aspell pipe" .. options)
-                        spellcheck(micro.CurPane())
-                    end
-                end
+    for _, misspell in ipairs(parseOutput(out)) do
+        if loc:GreaterEqual(misspell.mstart) and loc:LessEqual(misspell.mend) then
+            local options = ""
+            if buf.Settings["aspell.lang"] ~= "" then
+                options = options .. " --lang="
+                        .. buf.Settings["aspell.lang"]
             end
+            if buf.Settings["aspell.dict"] ~= "" then
+                options = options .. " --master="
+                        .. buf.Settings["aspell.dict"]
+            end
+            if buf.Settings["aspell.args"] ~= "" then
+                options = options .. " " .. buf.Settings["aspell.args"]
+            end
+
+            shell.ExecCommand("sh", "-c", "echo '*" .. misspell.word
+                    .. "\n#\n' | aspell pipe" .. options)
+            spellcheck(buf)
         end
     end
 end
 
-function split(inputstr, sep)
-    sep=sep or '%s'
-    local t={}
-    for field,s in string.gmatch(inputstr, "([^"..sep.."]*)("..sep.."?)") do
-        table.insert(t,field)
-        if s=="" then
-            return t
+function split(str, pat)
+    local t = {}
+    local fpat = "(.-)" .. pat
+    local last_end = 1
+    local s, e, cap = str:find(fpat, 1)
+    while s do
+        if s ~= 1 or cap ~= "" then
+            table.insert(t, cap)
         end
+        last_end = e+1
+        s, e, cap = str:find(fpat, last_end)
     end
+    if last_end <= #str then
+        cap = str:sub(last_end)
+        table.insert(t, cap)
+    end
+    return t
 end
 
 -- We need to spellcheck every time, the buffer is modified. Sadly there's
 -- no such thing as onBufferModified()
 
-function onBufPaneOpen(bp)
-    spellcheck(bp)
+function onBufferOpen(buf)
+    spellcheck(buf)
 end
 
 function onRune(bp)
-    spellcheck(bp)
+    spellcheck(bp.Buf)
 end
 
 function onPastePrimary(bp) -- I'm not sure if this exists
-    spellcheck(bp)
+    spellcheck(bp.Buf)
 end
 
 -- The following were copied from help keybindings
 
-function onCursorUp(bp)
-end
+-- function onCursorUp(bp)
+-- end
 
-function onCursorDown(bp)
-end
+-- function onCursorDown(bp)
+-- end
 
-function onCursorPageUp(bp)
-end
+-- function onCursorPageUp(bp)
+-- end
 
-function onCursorPageDown(bp)
-end
+-- function onCursorPageDown(bp)
+-- end
 
-function onCursorLeft(bp)
-end
+-- function onCursorLeft(bp)
+-- end
 
-function onCursorRight(bp)
-end
+-- function onCursorRight(bp)
+-- end
 
-function onCursorStart(bp)
-end
+-- function onCursorStart(bp)
+-- end
 
-function onCursorEnd(bp)
-end
+-- function onCursorEnd(bp)
+-- end
 
-function onSelectToStart(bp)
-end
+-- function onSelectToStart(bp)
+-- end
 
-function onSelectToEnd(bp)
-end
+-- function onSelectToEnd(bp)
+-- end
 
-function onSelectUp(bp)
-end
+-- function onSelectUp(bp)
+-- end
 
-function onSelectDown(bp)
-end
+-- function onSelectDown(bp)
+-- end
 
-function onSelectLeft(bp)
-end
+-- function onSelectLeft(bp)
+-- end
 
-function onSelectRight(bp)
-end
+-- function onSelectRight(bp)
+-- end
 
-function onSelectToStartOfText(bp)
-end
+-- function onSelectToStartOfText(bp)
+-- end
 
-function onSelectToStartOfTextToggle(bp)
-end
+-- function onSelectToStartOfTextToggle(bp)
+-- end
 
-function onWordRight(bp)
-end
+-- function onWordRight(bp)
+-- end
 
-function onWordLeft(bp)
-end
+-- function onWordLeft(bp)
+-- end
 
-function onSelectWordRight(bp)
-end
+-- function onSelectWordRight(bp)
+-- end
 
-function onSelectWordLeft(bp)
-end
+-- function onSelectWordLeft(bp)
+-- end
 
 function onMoveLinesUp(bp)
-    spellcheck(bp)
+    spellcheck(bp.Buf)
 end
 
 function onMoveLinesDown(bp)
-    spellcheck(bp)
+    spellcheck(bp.Buf)
 end
 
 function onDeleteWordRight(bp)
-    spellcheck(bp)
+    spellcheck(bp.Buf)
 end
 
 function onDeleteWordLeft(bp)
-    spellcheck(bp)
+    spellcheck(bp.Buf)
 end
 
-function onSelectLine(bp)
-end
+-- function onSelectLine(bp)
+-- end
 
-function onSelectToStartOfLine(bp)
-end
+-- function onSelectToStartOfLine(bp)
+-- end
 
-function onSelectToEndOfLine(bp)
-end
+-- function onSelectToEndOfLine(bp)
+-- end
 
 function onInsertNewline(bp)
-    spellcheck(bp)
+    spellcheck(bp.Buf)
 end
 
 function onInsertSpace(bp)
-    spellcheck(bp)
+    spellcheck(bp.Buf)
 end
 
 function onBackspace(bp)
-    spellcheck(bp)
+    spellcheck(bp.Buf)
 end
 
 function onDelete(bp)
-    spellcheck(bp)
+    spellcheck(bp.Buf)
 end
 
-function onCenter(bp)
-    -- I dont know what this does
-end
+-- function onCenter(bp)
+-- end
 
 function onInsertTab(bp)
-    spellcheck(bp)
+    spellcheck(bp.Buf)
 end
 
-function onSave(bp)
-end
+-- function onSave(bp)
+-- end
 
-function onSaveAll(bp)
-end
+-- function onSaveAll(bp)
+-- end
 
-function onSaveAs(bp)
-end
+-- function onSaveAs(bp)
+-- end
 
-function onFind(bp)
-end
+-- function onFind(bp)
+-- end
 
-function onFindLiteral(bp)
-end
+-- function onFindLiteral(bp)
+-- end
 
-function onFindNext(bp)
-end
+-- function onFindNext(bp)
+-- end
 
-function onFindPrevious(bp)
-end
+-- function onFindPrevious(bp)
+-- end
 
 function onUndo(bp)
-    spellcheck(bp)
+    spellcheck(bp.Buf)
 end
 
 function onRedo(bp)
-    spellcheck(bp)
+    spellcheck(bp.Buf)
 end
 
-function onCopy(bp)
-end
+-- function onCopy(bp)
+-- end
 
-function onCopyLine(bp)
-end
+-- function onCopyLine(bp)
+-- end
 
 function onCut(bp)
-    spellcheck(bp)
+    spellcheck(bp.Buf)
 end
 
 function onCutLine(bp)
-    spellcheck(bp)
+    spellcheck(bp.Buf)
 end
 
 function onDuplicateLine(bp)
-    spellcheck(bp)
+    spellcheck(bp.Buf)
 end
 
 function onDeleteLine(bp)
-    spellcheck(bp)
+    spellcheck(bp.Buf)
 end
 
 function onIndentSelection(bp)
-    spellcheck(bp)
+    spellcheck(bp.Buf)
 end
 
 function onOutdentSelection(bp)
-    spellcheck(bp)
+    spellcheck(bp.Buf)
 end
 
 function onOutdentLine(bp)
-    spellcheck(bp)
+    spellcheck(bp.Buf)
 end
 
 function onIndentLine(bp)
-    spellcheck(bp)
+    spellcheck(bp.Buf)
 end
 
 function onPaste(bp)
-    spellcheck(bp)
+    spellcheck(bp.Buf)
 end
 
-function onSelectAll(bp)
-end
+-- function onSelectAll(bp)
+-- end
 
-function onOpenFile(bp)
-end
+-- function onOpenFile(bp)
+-- end
 
-function onStart(bp)
-end
+-- function onStart(bp)
+-- end
 
-function onEnd(bp)
-end
+-- function onEnd(bp)
+-- end
 
-function onPageUp(bp)
-end
+-- function onPageUp(bp)
+-- end
 
-function onPageDown(bp)
-end
+-- function onPageDown(bp)
+-- end
 
-function onSelectPageUp(bp)
-end
+-- function onSelectPageUp(bp)
+-- end
 
-function onSelectPageDown(bp)
-end
+-- function onSelectPageDown(bp)
+-- end
 
-function onHalfPageUp(bp)
-end
+-- function onHalfPageUp(bp)
+-- end
 
-function onHalfPageDown(bp)
-end
+-- function onHalfPageDown(bp)
+-- end
 
-function onStartOfLine(bp)
-end
+-- function onStartOfLine(bp)
+-- end
 
-function onEndOfLine(bp)
-end
+-- function onEndOfLine(bp)
+-- end
 
-function onStartOfText(bp)
-end
+-- function onStartOfText(bp)
+-- end
 
-function onStartOfTextToggle(bp)
-end
+-- function onStartOfTextToggle(bp)
+-- end
 
-function onParagraphPrevious(bp)
-end
+-- function onParagraphPrevious(bp)
+-- end
 
-function onParagraphNext(bp)
-end
+-- function onParagraphNext(bp)
+-- end
 
-function onToggleHelp(bp)
-end
+-- function onToggleHelp(bp)
+-- end
 
-function onToggleDiffGutter(bp)
-end
+-- function onToggleDiffGutter(bp)
+-- end
 
-function onToggleRuler(bp)
-end
+-- function onToggleRuler(bp)
+-- end
 
-function onJumpLine(bp)
-end
+-- function onJumpLine(bp)
+-- end
 
-function onClearStatus(bp)
-end
+-- function onClearStatus(bp)
+-- end
 
-function onShellMode(bp)
-end
+-- function onShellMode(bp)
+-- end
 
-function onCommandMode(bp)
-end
+-- function onCommandMode(bp)
+-- end
 
-function onQuit(bp)
-end
+-- function onQuit(bp)
+-- end
 
-function onQuitAll(bp)
-end
+-- function onQuitAll(bp)
+-- end
 
-function onAddTab(bp)
-end
+-- function onAddTab(bp)
+-- end
 
-function onPreviousTab(bp)
-end
+-- function onPreviousTab(bp)
+-- end
 
-function onNextTab(bp)
-end
+-- function onNextTab(bp)
+-- end
 
-function onNextSplit(bp)
-end
+-- function onNextSplit(bp)
+-- end
 
-function onUnsplit(bp)
-end
+-- function onUnsplit(bp)
+-- end
 
-function onVSplit(bp)
-end
+-- function onVSplit(bp)
+-- end
 
-function onHSplit(bp)
-end
+-- function onHSplit(bp)
+-- end
 
-function onPreviousSplit(bp)
-end
+-- function onPreviousSplit(bp)
+-- end
 
-function onToggleMacro(bp)
-end
+-- function onToggleMacro(bp)
+-- end
 
 function onPlayMacro(bp)
-    spellcheck(bp)
+    spellcheck(bp.Buf)
 end
 
-function onSuspend(bp) -- Unix only
-end
+-- function onSuspend(bp) -- Unix only
+-- end
 
-function onScrollUp(bp)
-end
+-- function onScrollUp(bp)
+-- end
 
-function onScrollDown(bp)
-end
+-- function onScrollDown(bp)
+-- end
 
-function onSpawnMultiCursor(bp)
-end
+-- function onSpawnMultiCursor(bp)
+-- end
 
-function onSpawnMultiCursorUp(bp)
-end
+-- function onSpawnMultiCursorUp(bp)
+-- end
 
-function onSpawnMultiCursorDown(bp)
-end
+-- function onSpawnMultiCursorDown(bp)
+-- end
 
-function onSpawnMultiCursorSelect(bp)
-end
+-- function onSpawnMultiCursorSelect(bp)
+-- end
 
-function onRemoveMultiCursor(bp)
-end
+-- function onRemoveMultiCursor(bp)
+-- end
 
-function onRemoveAllMultiCursors(bp)
-end
+-- function onRemoveAllMultiCursors(bp)
+-- end
 
-function onSkipMultiCursor(bp)
-end
+-- function onSkipMultiCursor(bp)
+-- end
 
-function onNone(bp)
-end
+-- function onNone(bp)
+-- end
 
-function onJumpToMatchingBrace(bp)
-end
+-- function onJumpToMatchingBrace(bp)
+-- end
 
 function onAutocomplete(bp)
-    spellcheck(bp)
+    spellcheck(bp.Buf)
 end
-
